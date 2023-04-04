e8d00b9fbd2c44b148493f39e5b0751f4cf1d2e2
Michael Carlstrom <rmc@carlstrom.com>
2023-04-04 02:15:43 -0400

    fixed particle nonetype

--- Project2/particle.py
+++ Project2/particle.py
@@ -28,7 +28,7 @@ class Particle:
         else:
             self.set_pose(pose)
 
-        self.weight=None
+        self.weight=0.5
         self.measurements = []
         
     #use pg 478 as a reference for an overview of the full algorithm
