64bb0c064eda679a0f63b39ddf3ba6d33380397e
Michael Carlstrom <rmc@carlstrom.com>
2023-04-04 02:30:25 -0400

    added prints to main

--- Project2/main.py
+++ Project2/main.py
@@ -45,29 +45,13 @@ particle_samples=np.empty(num_particles,dtype=Particle)
 for n in range(num_particles):
     particle_samples[n]=Particle()
 
-# print(particle_samples[0].get_map())
-# vis = Visualization(particle_samples[0].get_map(), real_pose)
-
-'''
-while 0.5 in particle_samples[0].get_map():
-    l = nearest_list(particle_samples[0].get_map(), particle_samples[0].get_pose())
-    for i in range(len(l) - 1):
-        motions = l[i].get_motion(l[i + 1], delta_t)
-        if type(motions[0]) == float:
-            recieve_motion_command(motions)
-            # vis.update(particle_samples[0].get_map(), particle_samples[0].get_pose(), i % 40 == 0)
-            # vis.pause()
-        else:
-            recieve_motion_command(motions[0])
-            recieve_motion_command(motions[1])
-
-print("Mapped Maze!")
-'''
 
 def recieve_motion_command(u,particle_samples):
 
     #move measurement wizard according to command
     z=measure.navigate_maze(u,stepsize)
+
+    print("recieved measurment")
 
     #move particles according to command
     for i in range(num_particles):
@@ -78,11 +62,14 @@ def recieve_motion_command(u,particle_samples):
         particle_samples[i].set_measurements(z) #recieves measurement
         particle_samples[i].likelihood_field_range_finder_model() #measurement model
 
+    print("lots of stuff")
+
     #rejection sampling to see which robots survive -> this converges faster if I narrow down the range of my guesses
     maxweight=0
     for i in range(num_particles):
         if particle_samples[i].get_weight()>maxweight:
             maxweight=particle_samples[i].get_weight()
+    print("resampling 1")
 
     #implement rejection sampling
     new_samples=np.empty(num_particles,dtype=Particle)
@@ -98,6 +85,8 @@ def recieve_motion_command(u,particle_samples):
                 j=1
                 new_samples[i]=particle_samples[samplenumber] #I want to pass the address in memory not split the object
 
+    print("resampling 2")
+
     #update map, but update is expensive so only run if particle not seen before
     weightlog=np.zeros(num_particles)
     for i in range(num_particles):
@@ -105,9 +94,11 @@ def recieve_motion_command(u,particle_samples):
             new_samples[i].update_occupancy_grid()
             weightlog[i]=new_samples[i].get_weight()
 
-    #now I need to np.copy() the map in each particle to split the objects and make them independent
+    # now I need to np.copy() the map in each particle to split the objects and make them independent
     # for i in range(num_particles):
-    #     new_samples[i].setmap(new_samples[i].getmap().copy())
+    #     new_samples[i].set_map(new_samples[i].getmap().copy())
+
+    print("map update")
 
     #overwrite the old set of samples
     return new_samples 
@@ -120,6 +111,7 @@ while 0.5 in particle_samples[0].get_map():
 
     if len(l) == 1:
         particle_samples = recieve_motion_command([0, 0], particle_samples)
+        # vis.update(particle_samples[0].get_map(), particle_samples[0].get_pose())
 
     for i in range(len(l) - 1):
         motions = l[i].get_motion(l[i + 1], delta_t)
