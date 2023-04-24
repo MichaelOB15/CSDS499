d403f87bd5bc92046f8f03a7fe17c5daffabe13d
Michael Carlstrom <rmc@carlstrom.com>
2023-04-24 13:34:58 -0400

    can add edges

--- Project2/graph.py
+++ Project2/graph.py
@@ -14,33 +14,56 @@ class Graph():
         else:
             self.g[Point(point_or_x, y)] = []
 
+    def get_nodes(self):
+        return self.g.keys()
+
+    def get_neighbors(self, point_or_x: Union[Point, float], y: float = -1) -> List[Point]:
+        if isinstance(point_or_x, Point):
+            return self.g[point_or_x]
+        else:
+            return self.g[Point(point_or_x, y)]
+
     def add_vertex(self, p1: Point, p2: Point):
         self.g[p1].append(p2)
         self.g[p2].append(p1)
 
-    def add_vertexes(self, parent: Point, children: List[Point]):
+    def add_vertices(self, parent: Point, children: List[Point]):
         for p in children:
             self.add_vertex(parent, p)
 
     def __str__(self) -> str:
-        return self.g.__str__()
+        string: str = ""
+        for n in self.get_nodes():
+            string = string + f"{n} : ["
+            for neigbors in self.get_neighbors(n):
+                string = string + f'{neigbors}, '
+            string = string + "]\n"
+        return string
 
     def graph_vis(self):
 
-        points = self.g.keys()
         x: List[float] = []
         y: List[float] = []
-        for p in points:
+        for p in self.get_nodes():
+
+            for neighbors in self.get_neighbors(p):
+                plt.plot([p.x, neighbors.x], [p.y, neighbors.y])
+
             x.append(p.x)
             y.append(p.y)
 
         plt.scatter(x, y)
         plt.show()
 
+
 g = Graph()
 g.add_node(0, 0)
-g.add_node(1, 0)
+g.add_node(Point(1, 0))
 g.add_node(1, 1)
 g.add_node(0, 1)
+
+
+g.add_vertex(Point(1, 0), Point(0, 1))
+g.add_vertices(Point(1, 1), [Point(0, 0), Point(0, 1), Point(1, 0)])
 
 g.graph_vis()
--- Project2/point.py
+++ Project2/point.py
@@ -16,3 +16,14 @@ class Point():
         dx = self.x - other.x
         dy = self.y - other.y
         return hypot(dx, dy)
+
+    def midpoint(self, other):
+        midx = self.x - other.x/2
+        midy = self.y - other.y/2
+        return Point(midx, midy)
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __eq__(self, other):
+        return (self.x, self.y) == (other.x, other.y)
