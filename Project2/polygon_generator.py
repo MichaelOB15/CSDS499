a4bda90f5d3dcf6a157deffacc083ab4c962ac11
GabeThaBabe <gjf24@case.edu>
2023-05-05 15:47:14 -0400

    asdfghjkl

--- Project2/polygon_generator.py
+++ Project2/polygon_generator.py
@@ -10,7 +10,12 @@ def plot_polygon(polygon, scaling, xshift, yshift):
     
     # so it is plotted as closed polygon
     polygon.append(polygon[0])
+    xs, ys = zip(*polygon)
+    
 
+    plt.plot(xs, ys, "k-", linewidth=1)
+
+def reformat(polygon, scaling, xshift, yshift):
     xs, ys = zip(*polygon)
     x=[]
     y=[]
@@ -20,18 +25,23 @@ def plot_polygon(polygon, scaling, xshift, yshift):
     for i in range(len(ys)):
         y.append(ys[i]*scaling+yshift)
 
-    plt.plot(x, y, "k-", linewidth=1)
+    polygon2=[]
+    for i in range(len(x)):
+        polygon2.append([x[i],y[i]])
+
+    return polygon2
 
 
-# this is just so that you can reproduce the same results
-random.seed(3)
+random.seed(3) # this is to reproduce the same results every time
 
-plt.figure()  #plt.savefig(out_file_name, dpi=300)
+#set up plot
+plt.figure()
 plt.gca().set_aspect("equal")
 plt.xlim([-10,10])
 plt.ylim([-10,10])
 
-polygon = random_convex_polygon(num_points=7) #outer boundary
+#set up shapes
+polygon=random_convex_polygon(num_points=7) #outer boundary
 polygon2=random_star_shaped_polygon(num_points=12) #inner obstacles
 polygon3=random_star_shaped_polygon(num_points=12)
 polygon4=random_star_shaped_polygon(num_points=12)
@@ -41,10 +51,15 @@ xshift=[-10,0,-8,2]
 yshift=[-10,0,0,-8]
 pg=[polygon,polygon2,polygon3,polygon4]
 
-for i in range(len(scaling)):
-    plot_polygon(pg[i],scaling[i],xshift[i],yshift[i])
+output=[]
 
-plt.show()
+for i in range(len(scaling)):
+    output.append(reformat(pg[i],scaling[i],xshift[i],yshift[i]))
+
+for i in range(len(scaling)):
+    plot_polygon(output[i],scaling[i],xshift[i],yshift[i])
+
+plt.show() #plt.savefig(out_file_name, dpi=300)
 
 
 #print(polygon[0][1])
