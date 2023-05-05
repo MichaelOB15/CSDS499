396ac0b54d8055069f7f502a30dd415b387ec9f1
MichaelOB15 <52471264+MichaelOB15@users.noreply.github.com>
2023-05-05 12:53:33 -0400

    added polygenerator boiler plate code and config file

--- Project2/main.py
+++ Project2/main.py
@@ -1,5 +1,8 @@
 
-def main():
+def main(config):
+
+    # polygon generator pass in the config
+        # spit out polygons with verticies and the dimmensions of the space
 
     # first make the obstacle
 
@@ -13,5 +16,15 @@ def main():
 
     pass
 
+def load_config():
+    config_filepath = Path.cwd() / "config.yaml"
+    with config_filepath.open() as f:
+        config_dict = yaml.load(f, Loader=yaml.FullLoader)
+    config = argparse.Namespace()
+    for key, value in config_dict.items():
+        setattr(config, key, value)
+    return config
+
 if __name__ == "__main__":
-   main()
+    config = load_config()
+    main(config)
