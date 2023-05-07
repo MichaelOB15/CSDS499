d7b8306f0d8360a6724a4b0a16094ad1b730e397
Michael Carlstrom <rmc@carlstrom.com>
2023-05-07 15:03:34 -0400

    removed test

--- Project2/brushfire_decomposition.py
+++ Project2/brushfire_decomposition.py
@@ -17,7 +17,6 @@ class Brushfire():
         self.map: List[List[int]] = self.generate_map()
 
     def generate_map(self):
-        return
         #make a 2d list that will fit the whole space
         numPts=len(self.boundary)
 
@@ -163,11 +162,6 @@ class Brushfire():
 
 def test():
     bf = Brushfire(1, 2)
-    bf.map = [[0, 0, 0, 0, 0],
-              [1, 1, 1, 1, 0],
-              [0, 0, 0, 0, 0],
-              [0, 1, 1, 1, 1],
-              [0, 0, 0, 0, 0]]
     bf.wavefront(Point(5, 0))
 
 
