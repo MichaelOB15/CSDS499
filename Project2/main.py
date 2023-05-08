94c50c64c1fa2a0132455901e034993d2af5366e
Michael Carlstrom <rmc@carlstrom.com>
2023-05-08 11:54:27 -0400

    commmented out broken code

--- Project2/main.py
+++ Project2/main.py
@@ -7,81 +7,72 @@ from pathlib import Path
 import yaml
 import argparse
 
-from PIL import Image
-import numpy as np
-
-import matplotlib.pyplot as plt
 
 def main(config):
 
     map = Workspace()
     obj = map.gen()
 
-    #trap_decom = TD(obj[0], obj[1], obj[2], obj[3])
-    #graph = trap_decom.calculate_nodes()
-    #trap_decom.vis()
+    trap_decom = TD(obj[0], obj[1], obj[2], obj[3])
+    graph = trap_decom.calculate_nodes()
+    trap_decom.vis()
 
-    #graph.graph_vis()
+    graph.graph_vis()
     start = Point(obj[2][0], obj[2][1])
     end = Point(obj[3][0], obj[3][1])
-    #graph.path_graph(start, end, EUCLIDEAN)
-    
+    graph.path_graph(start, end, EUCLIDEAN)
 
-    bf = Brushfire(obj[0],obj[1],obj[2],obj[3])
-    img=bf.brushfireAlg()
-    
-    bf.wavefront()
-    val=bf.get_map()
+    # bf = Brushfire(obj[0],obj[1],obj[2],obj[3])
+    # img=bf.brushfireAlg()
 
-    [ind1,ind2]=[len(val),len(val[0])]
-    #img=np.zeros([ind1,ind2])
-    for x in range(ind1):
-        for y in range(ind2):
-            img[x,y]=val[x][y]/4-img[x,y]
+    # bf.wavefront()
+    # val=bf.get_map()
 
-    print(img[obj[2][0],obj[2][1]])
+    # [ind1,ind2]=[len(val),len(val[0])]
+    # #img=np.zeros([ind1,ind2])
+    # for x in range(ind1):
+    #     for y in range(ind2):
+    #         img[x,y]=val[x][y]/4-img[x,y]
 
-    nodes=[]
-    nodes.append(obj[2])
-    while(not nodes[-1]==obj[3]):
-        x=nodes[-1][0]
-        y=nodes[-1][1]
+    # print(img[obj[2][0],obj[2][1]])
 
-        min=float('inf')
-        coord=[0,0]
+    # nodes=[]
+    # nodes.append(obj[2])
+    # while(not nodes[-1]==obj[3]):
+    #     x=nodes[-1][0]
+    #     y=nodes[-1][1]
 
-        for a in range(3):
-            for b in range(3):
-                if(img[a,b]<min):
-                    
-                    min=img[a,b]
-                    coord=[a,b]
-        print(coord)
-        nodes.append[coord]
+    #     min=float('inf')
+    #     coord=[0,0]
 
+    #     for a in range(3):
+    #         for b in range(3):
+    #             if(img[a,b]<min):
+    #                 min=img[a,b]
+    #                 coord=[a,b]
+    #     print(coord)
+    #     nodes.append(coord)
+    #     print(nodes)
 
-        #set up plot
-        plt.figure()
-        plt.gca().set_aspect("equal")
-        plt.xlim([-10,10])
-        plt.ylim([-10,10])
+    #     #set up plot
+    #     plt.figure()
+    #     plt.gca().set_aspect("equal")
+    #     plt.xlim([-10,10])
+    #     plt.ylim([-10,10])
 
-        x = []
-        y = []
-        for i in range(len(nodes)):
-            x.append(nodes[i][0])
-            y.append(nodes[i][1])
-        plt.plot(x, y)
-        plt.show()
-            # self.plot_polygon(output[i])
+    #     x = []
+    #     y = []
+    #     for i in range(len(nodes)):
+    #         x.append(nodes[i][0])
+    #         y.append(nodes[i][1])
+    #     plt.plot(x, y)
+    #     # plt.show()
+    #         # self.plot_polygon(output[i])
 
 
-        plt.show() #plt.savefig(out_file_name, dpi=300)
+    # plt.show() #plt.savefig(out_file_name, dpi=300)
         #print(polygon[0][1])
-
-    
-
-    
+   
     #img = Image.fromarray(img.astype('uint8')*5)
     #img.show()
     #img.save("brushfire.png")
