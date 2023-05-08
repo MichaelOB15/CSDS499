a15450ae1c3d52dfd8d99fa6f229fe1ca5c7cf2c
GabeThaBabe <gjf24@case.edu>
2023-05-08 11:19:28 -0400

    sdfgh

--- Project2/main.py
+++ Project2/main.py
@@ -10,20 +10,21 @@ import argparse
 from PIL import Image
 import numpy as np
 
+import matplotlib.pyplot as plt
 
 def main(config):
 
     map = Workspace()
     obj = map.gen()
 
-    trap_decom = TD(obj[0], obj[1], obj[2], obj[3])
-    graph = trap_decom.calculate_nodes()
-    trap_decom.vis()
+    #trap_decom = TD(obj[0], obj[1], obj[2], obj[3])
+    #graph = trap_decom.calculate_nodes()
+    #trap_decom.vis()
 
-    graph.graph_vis()
+    #graph.graph_vis()
     start = Point(obj[2][0], obj[2][1])
     end = Point(obj[3][0], obj[3][1])
-    graph.path_graph(start, end, EUCLIDEAN)
+    #graph.path_graph(start, end, EUCLIDEAN)
     
 
     bf = Brushfire(obj[0],obj[1],obj[2],obj[3])
@@ -36,12 +37,54 @@ def main(config):
     #img=np.zeros([ind1,ind2])
     for x in range(ind1):
         for y in range(ind2):
-            img[x,y]=img[x,y]+val[x][y]/8
+            img[x,y]=val[x][y]/4-img[x,y]
+
+    print(img[obj[2][0],obj[2][1]])
+
+    nodes=[]
+    nodes.append(obj[2])
+    while(not nodes[-1]==obj[3]):
+        x=nodes[-1][0]
+        y=nodes[-1][1]
+
+        min=float('inf')
+        coord=[0,0]
+
+        for a in range(3):
+            for b in range(3):
+                if(img[a,b]<min):
+                    
+                    min=img[a,b]
+                    coord=[a,b]
+        print(coord)
+        nodes.append[coord]
+
+
+        #set up plot
+        plt.figure()
+        plt.gca().set_aspect("equal")
+        plt.xlim([-10,10])
+        plt.ylim([-10,10])
+
+        x = []
+        y = []
+        for i in range(len(nodes)):
+            x.append(nodes[i][0])
+            y.append(nodes[i][1])
+        plt.plot(x, y)
+        plt.show()
+            # self.plot_polygon(output[i])
+
+
+        plt.show() #plt.savefig(out_file_name, dpi=300)
+        #print(polygon[0][1])
 
     
-    img = Image.fromarray(img.astype('uint8')*5)
-    img.show()
-    img.save("brushfire.png")
+
+    
+    #img = Image.fromarray(img.astype('uint8')*5)
+    #img.show()
+    #img.save("brushfire.png")
     # img = Image.fromarray(img.astype('uint8'))
     # img.show()
     # img.save("brushfire.png")
